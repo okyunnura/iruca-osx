c2dee54a2889bf36d5cbdd832f9dbffd873049e7
okumura <ken@tagbangers.co.jp>
2017-01-20 00:36:00 +0900

    scaffolding

--- iruca-osx/ViewController.swift
+++ iruca-osx/ViewController.swift
@@ -1,6 +1,15 @@
 import Cocoa
 
 class ViewController: NSViewController {
+	
+	@IBOutlet weak var popUpButton: NSPopUpButton!
+	
+	@IBOutlet weak var idTextField: NSTextField!
+	
+	@IBOutlet weak var nameTextField: NSTextField!
+	
+	@IBOutlet weak var messageTextField: NSTextField!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
