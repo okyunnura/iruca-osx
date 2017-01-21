41c2d5eeb97c8e6190f97e5547bdb76f3b69f7c5
okumura <ken@tagbangers.co.jp>
2017-01-21 22:37:10 +0900

    scaffolding

--- iruca-osx/ViewController.swift
+++ iruca-osx/ViewController.swift
@@ -1,6 +1,10 @@
 import Cocoa
+import XCGLogger
 
 class ViewController: NSViewController {
+	private let logger = XCGLogger.default
+	
+	let array = ["在席", "離席", "外出", "休暇", "電話中", "打ち合わせ中", "退社"]
 	
 	@IBOutlet weak var popUpButton: NSPopUpButton!
 	
@@ -12,7 +16,7 @@ class ViewController: NSViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		popUpButton.addItems(withTitles: array)
 	}
 
 	override var representedObject: Any? {
